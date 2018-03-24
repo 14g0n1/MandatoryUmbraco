0eaf2adfbdf3db371545cb87170cf75d2f49cea0
Rasmus Lagoni <rasmus191190@live.dk>
2018-03-24 13:31:05 +0100

    Init commit

---
+++ MandatoryWORKING/Controllers/ContactPageSurfaceController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Core.Models;
+using Umbraco.Web.Mvc;
+using umbracoInitializer.ViewModels;
+
+namespace umbracoInitializer.Controllers
+{
+    public class ContactPageSurfaceController : SurfaceController
+    {
+        // GET: ContactPageSurface
+        public ActionResult Index()
+        {
+            return PartialView("ContactPagePartial", new ContactForm());
+        }
+
+        [HttpPost]
+        public ActionResult SubmitForm(ContactForm contactForm) {
+            if (!ModelState.IsValid) { return CurrentUmbracoPage(); }
+
+
+            //instansiating mailmessage object with posted values
+            MailMessage mail = new MailMessage()
+            {
+                Subject = contactForm.Subject,
+                From = new MailAddress(contactForm.Email, contactForm.Name),
+                Body = contactForm.Message
+            };
+
+            mail.To.Add("eaarhl@eaaa.dk");
+
+            //places mail on c driv under c:/TEMP/mail
+            try { 
+            using(SmtpClient smtp = new SmtpClient())
+            {
+                smtp.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+                smtp.PickupDirectoryLocation = "C:\\TEMP\\Mail";
+                smtp.UseDefaultCredentials = false;
+                smtp.EnableSsl = false;
+              //  smtp.Host = "smtp.gmail.com";
+               // smtp.Port = 587;
+                smtp.Credentials = new NetworkCredential("rhl@miracle.dk", "1234");
+
+                smtp.Send(mail);
+            }
+            }catch(Exception e)
+            {
+                //You would here log the error and return a custom error message to the client, if an exception was thrown
+            }
+
+            //create the message node in the backoffice with the subject as 
+            //first instansiate the node object and set values
+            IContent message = Services.ContentService.CreateContent(contactForm.Subject, CurrentPage.Id,"message");
+
+            message.SetValue("messageName", contactForm.Name);
+            message.SetValue("email", contactForm.Email);
+            message.SetValue("messageContent", contactForm.Message);
+            message.SetValue("subject", contactForm.Subject);
+
+            //save the node
+            Services.ContentService.Save(message);
+
+            //If process 
+            TempData["Success"] = true;
+            return CurrentUmbracoPage();
+        }
+    }
+}
---
+++ MandatoryWORKING/ViewModels/ContactForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace umbracoInitializer.ViewModels
+{
+    public class ContactForm
+    {
+        [Required(ErrorMessage = "Please enter a name!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter an email adress!")]
+        [RegularExpression("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
+            ErrorMessage ="Please enter a valid email!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter a subject!")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Please write a message!")]
+        public string Message { get; set; }
+    }
+}
